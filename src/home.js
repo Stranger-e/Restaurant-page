76e8d637a29acf83ac1150f2a9dcb18e229de51f
Ezekiel OLoo <olooezekiel885@gmail.com>
2024-06-04 19:00:57 +0300

    Add style and color to the homepage

--- src/home.js
+++ src/home.js
@@ -1,3 +1,13 @@
 export function startUp() {
-  console.log('I am being called from home.js');
+  const container = document.getElementById('content');
+
+  const header = document.createElement('h1');
+  header.innerHTML = 'Strangers Grill';
+  container.appendChild(header);
+
+  const intro = document.createElement('p');
+  intro.classList.add('intro');
+  intro.innerHTML =
+    "Get to dine with some Outstanding strangers, meal cooked by highly skilled and specialized strangers. You definetely won't miss a strange Face.";
+  container.appendChild(intro);
 }
