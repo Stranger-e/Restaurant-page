fcc8dcf84f6526e3e5a970ad7d9e42d082765a21
Ezekiel OLoo <olooezekiel885@gmail.com>
2024-06-05 15:57:33 +0300

    Final touch to the menu page & update the dist

--- src/menu.js
+++ src/menu.js
@@ -1,24 +1,25 @@
 class Card {
-  constructor(name, price, imageUrl) {
+  constructor(name, price, className) {
     this.name = name;
     this.price = price;
-    this.imageUrl = imageUrl;
+    this.className = className;
   }
 
   render() {
     const card = document.createElement('div');
-    card.classList.add('card');
+    card.classList.add('card', this.className);
 
-    card.style.backgroundImage = `url("${this.imageUrl}")`;
-    card.style.backgroundSize = 'cover';
+    const details = document.createElement('div');
+    details.classList.add('details');
+    card.appendChild(details);
 
     const nameElement = document.createElement('h3');
     nameElement.textContent = this.name;
-    card.appendChild(nameElement);
+    details.appendChild(nameElement);
 
     const priceElement = document.createElement('p');
     priceElement.textContent = `Price: $${this.price}`;
-    card.appendChild(priceElement);
+    details.appendChild(priceElement);
 
     return card;
   }
@@ -28,17 +29,12 @@ export function MenuItems() {
   const container = document.getElementById('content');
 
   const menuItemsData = [
-    {
-      name: 'Pasta Primavera',
-      price: 12.99,
-      imageUrl: './Assets/Chai.jpg',
-    },
-    {
-      name: 'Chicken Parmesan',
-      price: 15.99,
-      imageUrl: './Assets/Chapati.png',
-    },
-    { name: 'Caesar Salad', price: 8.99, imageUrl: './Assets/Mayai.jpg' },
+    { name: 'Pilau', price: 12.99, className: 'pilau' },
+    { name: 'Chapati', price: 15.99, className: 'chapati' },
+    { name: 'Mayai', price: 8.99, className: 'mayai' },
+    { name: 'Chai', price: 5.99, className: 'chai' },
+    { name: 'Kuku', price: 5.99, className: 'kuku' },
+    { name: 'Cabbage', price: 5.99, className: 'cabbage' },
   ];
 
   const display = document.createElement('div');
@@ -46,7 +42,7 @@ export function MenuItems() {
   container.appendChild(display);
 
   menuItemsData.forEach((itemData) => {
-    const card = new Card(itemData.name, itemData.price, itemData.imageUrl);
+    const card = new Card(itemData.name, itemData.price, itemData.className);
     display.appendChild(card.render());
   });
 }
